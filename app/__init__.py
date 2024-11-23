78ede817268c00303f7f24713e3f10181de04109
Niek Holter <n.e.holter@student.rug.nl>
2024-11-23 13:20:45 +0100

    implemented flask migration capabilities

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
@@ -7,10 +8,7 @@ app.config.from_object(Config)
 
 # Initialize the database
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Import routes and models
 from app import routes, models
-
-# Create the database and the tables
-with app.app_context():
-    db.create_all()
